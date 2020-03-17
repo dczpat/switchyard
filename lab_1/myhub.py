3115954d65a46a0b6b0ae8fa55b278e9aea17c36
pdon <donxcz@qq.com>
2020-03-17 21:24:18 +0800

    step2-new

--- lab_1/myhub.py
+++ lab_1/myhub.py
@@ -9,6 +9,9 @@ def main(net):
     my_interfaces = net.interfaces() 
     mymacs = [intf.ethaddr for intf in my_interfaces]
 
+    in_cnt=0
+    out_cnt=0
+
     while True:
         try:
             timestamp,dev,packet = net.recv_packet()
@@ -19,13 +22,18 @@ def main(net):
 
         log_debug ("In {} received packet {} on {}".format(net.name, packet, dev))
         eth = packet.get_header(Ethernet)
+        in_cnt++
+
         if eth is None:
             log_info("Received a non-Ethernet packet?!")
             continue
 
         if eth.dst in mymacs:
             log_info ("Received a packet intended for me")
+            log_info ("in:{} out:{}".format(in_cnt, out_cnt))
         else:
+            out_cnt+=len(my_interfaces)
+            log_info ("in:{} out:{}".format(in_cnt, out_cnt))
             for intf in my_interfaces:
                 if dev != intf.name:
                     log_info ("Flooding packet {} to {}".format(packet, intf.name))
