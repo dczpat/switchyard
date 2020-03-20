cd51070cb78369ea2078c8e3e9e8ee493fe00dbe
pdon <donxcz@qq.com>
2020-03-19 22:31:02 -0800

    Task 4 bug fixes

--- lab_2/myswitch_lru.py
+++ lab_2/myswitch_lru.py
@@ -12,9 +12,10 @@ import heapq
 def main(net):
     my_interfaces = net.interfaces() 
     mymacs = [intf.ethaddr for intf in my_interfaces]
-    max_rules=int(input("Please enter the maximum number of rules permitted: "))
-    # add an empty heapq to store  forwarding rules
+    max_rules = int(input("Please enter the maximum number of rules permitted: "))
+    # add an empty heapq to store forwarding rules
     # each element of tab should be a tuple like: (last_time, host, intf)
+    # when a heapq consists of tuples, it is organized based on the tuples' first elements
     tab = []
 
     while True:
@@ -26,40 +27,41 @@ def main(net):
             return
 
         log_debug ("In {} received packet {} on {}".format(net.name, packet, input_port))
-        
+        # src already recorded
         if packet[0].src in [rule[1] for rule in tab]:
             # find the corresbonding iterator for the current host
             cur = iter(tab)
             for rule in tab:
                 if rule[1] == packet[0].src:
-                    #if input_port == rule[2]:
                     cur = rule
                     break
-            # first delete, then add a new one    
-            del cur
-            #tab.insert((time.time(), packet[0].src, input_port))
-            #heapq.heapify(tab)
+            # first delete, then add a new one   
+            tab.remove(cur)
+        # src not recorded yet
         else:
-            if tab.len() == max_rules:
-            #    tab.insert((time.time(), packet[0].src, input_port))
-            #else:
+            # forwarding table is full
+            if len(tab) == max_rules:
                 heapq.heappop(tab)
-        tab.insert((time.time(), packet[0].src, input_port))
+        tab.append((time.time(), packet[0].src, input_port))
         heapq.heapify(tab)
 
+        # dst already recorded
         if packet[0].dst in [rule[1] for rule in tab]:
             # find the corresbonding iterator for the current host
             cur = iter(tab)
             for rule in tab:
-                if rule[1] == packet[0].src:
-                    #if input_port == rule[2]:
+                if rule[1] == packet[0].dst:
                     cur = rule
                     break
-            new_rule = [time.time(), cur[1], cur[2]]    
-            del cur
-            tab.insert(new_rule)
+            log_debug ("Flooding packet {} to {}".format(packet, cur[2]))
+            net.send_packet(cur[2], packet)
+            new_rule = (time.time(), cur[1], cur[2])
+            tab.remove(cur)
+            tab.append(new_rule)
             heapq.heapify(tab)
+        # dst not recorded yet    
         else:
+            # do nothing if dst is the switch itself
             if packet[0].dst not in mymacs:
                 for intf in my_interfaces:
                     if input_port != intf.name:
