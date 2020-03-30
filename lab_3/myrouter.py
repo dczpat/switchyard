2d8cafb9b08dce797159b15b240930b5f5cba9ad
pdon <donxcz@qq.com>
2020-03-29 21:12:12 -0800

    Task 2

--- lab_3/myrouter.py
+++ lab_3/myrouter.py
@@ -13,12 +13,17 @@ class Router(object):
     def __init__(self, net):
         self.net = net
         # other initialization stuff here
+        self.intfs = net.interfaces()
+        # init ipaddrs of all intfs
+        self.ipaddrs=[]
+        for intf in self.intfs:
+            self.ipaddrs.append(intf.ipaddr)
 
 
     def router_main(self):    
         '''
-        Main method for router; we stay in a loop in this method, receiving
-        packets until the end of time.
+        Main method for router; we stay in a loop in this method, 
+        receiving packets until the end of time.
         '''
         while True:
             gotpkt = True
@@ -33,6 +38,14 @@ class Router(object):
 
             if gotpkt:
                 log_debug("Got a packet: {}".format(str(pkt)))
+                # drop this time if it's not an ARP
+                if pkt.has_header(Arp):
+                    arp = pkt.get_header_by_name(Arp)
+                    # drop if it's not a request or target ip does not exist here
+                    if (arp.operation == ArpOperation.Request) and (arp.targetprotoaddr in self.ipaddrs):
+                        wanted_macaddr = self.net.interface_by_ipaddr(arp.targetprotoaddr).ethaddr
+                        arp_reply = create_ip_arp_reply(wanted_macaddr, arp.senderhwaddr, arp.targetprotoaddr, arp.senderprotoaddr)
+                        self.net.send_packet(dev, arp_reply)
 
 
 
