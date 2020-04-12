3033fb467c8e69767d42e70f6924a47147d6699d
pdon <donxcz@qq.com>
2020-04-11 23:25:03 -0800

    Task 2

--- lab_4/myrouter.py
+++ lab_4/myrouter.py
@@ -8,6 +8,7 @@ import sys
 import os
 import time
 from switchyard.lib.userlib import *
+from switchyard.lib.address import *
 
 class Router(object):
     def __init__(self, net):
@@ -19,8 +20,33 @@ class Router(object):
         for intf in self.intfs:
             self.ipaddrs.append(intf.ipaddr)
         # init the ARP table----IP-{MAC,last_time}
-        self.tab = {}
+        self.arp_tab = {}
+        # init the forwarding table
+        self.fwd_tab = []
 
+    def init_fwd_tab(self):
+        '''
+        Initialize the forwarding table from 2 sources:
+            1. the router's own interfaces
+            2. the file named 'forwarding_table.txt'
+        A simple list is used to hold each entry in the following order:
+            1. network address
+            2. subnet mask
+            3. next hop IP
+            4. intf to forward the packet
+        '''
+        # add src 1
+        for intf in self.intfs:
+            entry = []
+            entry[0] = intf.ipaddr
+            entry[1] = intf.netmask
+            entry[2] = '0.0.0.0' # next hop is NONE in this case
+            entry[3] = intf.name 
+
+        # add src 2
+        for line in open("forwarding_table.txt"):
+            entry = line.split()
+            self.fwd_tab.append(entry)
 
     def router_main(self):    
         '''
@@ -40,23 +66,42 @@ class Router(object):
 
             if gotpkt:
                 log_debug("Got a packet: {}".format(str(pkt)))
+                # 1. handle ARP packets
                 arp = pkt.get_header(Arp)
-                # drop this time if it's not an ARP request
-                if (arp) and (arp.operation == ArpOperation.Request):
-                    # add a new entry into the table or just update a recorded one
-                    self.tab[arp.senderprotoaddr] = {'mac': arp.senderhwaddr, 'last': time.time()}
-                    # delete old entries after 20s of idleness
-                    for host in list(self.tab):
-                        if time.time() - self.tab[host]['last'] > 20:
-                            del self.tab[host]
-                    log_info("Cached ARP table updated: {}".format(str(self.tab)))
+                if (arp):
+                    if (arp.operation == ArpOperation.Request):
+                        # add a new entry into the table or just update a recorded one
+                        self.arp_tab[arp.senderprotoaddr] = {'mac': arp.senderhwaddr, 'last': time.time()}
+                        log_info("Cached ARP table updated: {}".format(str(self.arp_tab)))
 
-                    # drop if target ip does not exist here
-                    if arp.targetprotoaddr in self.ipaddrs:
-                        wanted_macaddr = self.net.interface_by_ipaddr(arp.targetprotoaddr).ethaddr
-                        arp_reply = create_ip_arp_reply(wanted_macaddr, arp.senderhwaddr, arp.targetprotoaddr, arp.senderprotoaddr)
-                        self.net.send_packet(dev, arp_reply)
-
+                        # drop if target ip does not exist here
+                        if arp.targetprotoaddr in self.ipaddrs:
+                            wanted_macaddr = self.net.interface_by_ipaddr(arp.targetprotoaddr).ethaddr
+                            arp_reply = create_ip_arp_reply(wanted_macaddr, arp.senderhwaddr, arp.targetprotoaddr, arp.senderprotoaddr)
+                            self.net.send_packet(dev, arp_reply)
+                    elif (arp.operation == ArpOperation.Reply):
+                        # 
+                        a = 1 #delete
+                    continue
+                # 2. handle IPv4 packets
+                ipv4 = pkt.get_header(IPv4)
+                if (ipv4):
+                    # drop the pkt if the dst belong to the router itself
+                    if ipv4.dst not in self.ipaddrs:
+                        # denote the longest prefix match so far
+                        longest = 0
+                        # denote the matched entry so far
+                        matched_entry = []
+                        for entry in self.fwd_tab:
+                            prefixnet = IPv4Network(entry[0] + '/' + entry[1])
+                            if (ipv4.dst in prefixnet) and (longest < prefixnet.prefixlen):
+                                longest = prefixnet.prefixlen
+                                matched_entry = entry
+                        # drop the pkt if no matches found
+                        if matched_entry != [] :
+                            # 1. the dst is within the subnet to which the intf belong
+                            if matched_entry[2] == '0.0.0.0':
+                                
 
 
 def main(net):
@@ -65,5 +110,6 @@ def main(net):
     object and get it going.
     '''
     r = Router(net)
+    r.init_fwd_tab()
     r.router_main()
     net.shutdown()
